a7992f78b27ecfff8fa8d4a166c5fcbc8229633a
evgenij <mwbox89@gmail.com>
2021-03-08 00:45:08 +0200

    done sections team, delicious, events, specialties, book, about

--- app/js/app.js
+++ app/js/app.js
@@ -10,4 +10,35 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	// Custom JS
 
+
+	function toggleMenuCategory () {
+		let menuCategories = document.querySelector('.categories')
+		let menuItems = document.querySelectorAll('.menu__section')
+		
+		menuCategories.addEventListener('click', function(e) {
+			if (e.target.classList.contains('categories__btn')) {
+				changeCategory(e.target.dataset.toggle, menuItems)
+			}
+		})
+	}
+	function changeCategory (category, sections) {
+		
+		sections.forEach((item, index)=>{
+			if (item.dataset.target === category) {
+				item.style.display = 'flex'
+				setTimeout(function(){
+					item.classList.add('active')
+				}, 200)
+				
+			} else {
+				item.classList.remove('active')
+				setTimeout(function(){
+					item.style.display = ''
+				}, 200)
+			}
+		})
+	}
+
+	toggleMenuCategory()
+
 })
